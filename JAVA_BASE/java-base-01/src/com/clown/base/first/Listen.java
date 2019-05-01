a7317332c58849835519b463d4c49a69de6c47af
Richard <Richard.369.Ackerman@gmail.com>
2019-05-01 19:38:51 +0800

    First

---
+++ JAVA_BASE/java-base-01/src/com/clown/base/first/Listen.java
@@ -0,0 +1,7 @@
+package com.clown.base.first;
+
+public class Listen {
+    public static void main(String[] args) {
+        System.out.println("Excuses are like assholes.Everyone has one!!!");
+    }
+}
